2c3fd1f2b78b0827270daf4b0550fbd14d4b58f6
Sachin Roy <sroy895@gmail.com>
2019-03-12 23:37:41 +0530

    Create reverseK.cpp
    This code reverses elements of list in groups of K

---
+++ Algo++/LinkedList/reverseK.cpp
@@ -0,0 +1,49 @@
+#include<iostream>
+using namespace std;
+
+class node {
+public:
+    int data;
+    node* next;
+    node(int d){
+        data = d;
+        next = NULL;
+    }
+};
+
+node* reverseK (node *head, int k) {
+  node* current = head;
+  node* next = NULL;
+  node* prev = NULL;
+  int count = 0;
+
+  while (current != NULL && count < k) {
+    next = current->next;
+    current->next = prev;
+    prev = current;
+    current = next;
+    count++;
+  }
+  if (next != NULL)
+    head->next = reverseK(next, k);
+  return prev;
+}
+
+int main() {
+  int n,k,d;
+  node* head;
+  cin>>n>>k>>d;
+  head = new node(d);
+  node* temp = head;
+  for(int i = 1;i < n;i++) {
+    cin>>d;
+    temp->next = new node(d);
+    temp = temp->next;
+  }
+  head = reverseK(head,k);
+  while(head != NULL) {
+    cout<<head->data<<" ";
+    head = head->next;
+  }
+	return 0;
+}
