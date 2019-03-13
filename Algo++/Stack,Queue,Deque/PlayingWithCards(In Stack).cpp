a6244fd6e7aa1d265c109c469404c8b388bd5933
Sachin Roy <sroy895@gmail.com>
2019-03-14 00:17:15 +0530

    Update PlayingWithCards(In Stack).cpp

--- Algo++/Stack,Queue,Deque/PlayingWithCards(In Stack).cpp
+++ Algo++/Stack,Queue,Deque/PlayingWithCards(In Stack).cpp
@@ -1,4 +1,11 @@
 //AbhiG
+/*
+You are at a casino. There are N stacked cards on pile . Each card has a number written on it. Then there will be Q iterations. In ith
+iteration, you start picking up the cards in Ai-1th pile from the top one by one and check whether the number written on the card is
+divisible by the ith prime number. If the number is divisible, you stack that card on pile Bi. Otherwise, you stack that card on pile
+Ai. After Q iterations, cards can only be on pile B1, B2, B3, . . . BQ, AQ . Output numbers on these cards from top to bottom of each
+piles in order of B1, B2, B3, . . . BQ, AQ .
+*/
 #include<iostream>
 #include<stack>
 using namespace std;
