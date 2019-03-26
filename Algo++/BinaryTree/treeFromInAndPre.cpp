07605d69a0435a891ae571ca526a9f89f15b6b12
Sachin Roy <sroy895@gmail.com>
2019-03-26 21:33:55 +0530

    Update treeFromInAndPre.cpp

--- Algo++/BinaryTree/treeFromInAndPre.cpp
+++ Algo++/BinaryTree/treeFromInAndPre.cpp
@@ -1,21 +1,20 @@
 //Manal
+//Complete this code
 #include <iostream>
 using namespace std;
 
 class node{
-    public:
-        int data;
-        node*left;
-        node*right;
+public:
+    int data;
+    node*left;
+    node*right;
 
-        node(int d){
-            data = d;
-            left = NULL;
-            right = NULL;
-        }
+    node(int d){
+        data = d;
+        left = NULL;
+        right = NULL;
+    }
 };
-
-
 
 node* createTreeFromTrav(int *in,int *pre,int s,int e){
     static int i = 0;
@@ -29,8 +28,8 @@ node* createTreeFromTrav(int *in,int *pre,int s,int e){
     int index=-1;
     for(int j=s;s<=e;j++){
         if(in[j]==pre[i]){
-            index = j;
-            break;
+        index = j;
+        break;
         }
     }
 
@@ -40,10 +39,19 @@ node* createTreeFromTrav(int *in,int *pre,int s,int e){
     return root;
 }
 
-
-
-
-
 int main() {
-    cout<<"Hello World!";
+    int n,m;
+    cin>>n;
+    int *in = new int[n];
+    //int in[n];
+    for(int i = 0 ;i < n;i++) {
+        cin>>in[i];
+    }
+    cin>>m;
+    int *pre = new int[m];
+    //int pre[m];
+    for(int i = 0 ;i < m;i++) {
+        cin>>pre[i];
+    }
+    return 0;
 }
